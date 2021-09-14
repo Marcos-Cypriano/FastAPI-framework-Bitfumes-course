bb4528e281acee65b2a2aa4a4f871724be55bc1d
Marcos-Cypriano <ma_cypriano@hotmail.com>
2021-09-14 12:29:52 -0300

    Added get paths

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,3 +10,27 @@ def index():
 @app.get('/about')
 def about():
     return {'data': 'About your page'}
+
+@app.get('/blog')
+def blog(limit = 10, published: bool = True, sort: Optional[str] = None ):
+    #Only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blogs from DB'}
+    else:
+        return {'data': f'{limit} blogs from DB'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    #fetch blog unpublished
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    #fetch blog with id = id
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    #fetch comments from blog with id = id
+    return {'data': { 'comments': id }}
